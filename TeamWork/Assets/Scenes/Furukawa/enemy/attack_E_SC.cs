9631de0c069133d8b34b5b38c4bf6cdcbce23a10
Furukawa kouki <k020g1198@g.neec.ac.jp>
2024-02-04 21:54:28 +0900

    突進する雑魚敵実装

--- TeamWork/Assets/Scenes/Furukawa/enemy/attack_E_SC.cs
+++ TeamWork/Assets/Scenes/Furukawa/enemy/attack_E_SC.cs
@@ -1,3 +1,4 @@
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,15 +15,19 @@ public class attack_E_SC : MonoBehaviour
     public int maxMoveCount = 200;
     public float moveSpeed = 0.01f;
 
+    private Animator anim;
+    private AnimatorStateInfo animState;
     private enemyState myState = enemyState.idle;
     private GameObject player = null;
     private Rigidbody rbody;
     private Vector3 forPlayer;
+    private Vector3 rotation;
     //ë“ã@
     private bool isTurn = false;
     private int moveCount = 0;
     //ìÀêi
     private bool isAttack = false;
+    private bool isFind = false;
     private Vector3 goalPos;
 
     // Start is called before the first frame update
@@ -34,68 +39,31 @@ public class attack_E_SC : MonoBehaviour
         }
 
         rbody = GetComponent<Rigidbody>();
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        animState = anim.GetCurrentAnimatorStateInfo(0);
+
         //playerÇ∆ÇÃãóó£ÇåvéZ
         forPlayer = player.transform.position - transform.position;
 
-        if (myState == enemyState.idle)
-        {
-            Vector3 pos = transform.position;
+        //î≠å©Ç‹Ç≈
+        idle();
 
-            moveCount++;
-            if (moveCount > maxMoveCount)
-            {
-                isTurn = !isTurn;
-                moveCount = 0;
-            }
-
-            if (isTurn)
-            {
-                pos.x += moveSpeed;
-            }
-            else
-            {
-                pos.x -= moveSpeed;
-            }
-
-            transform.position = pos;
-
-            //çıìGÇ…à¯Ç¡Ç©Ç©Ç¡ÇΩÇÁ
-            if (forPlayer.magnitude <= 10.0f)
-            {
-                myState = enemyState.Attack;
-                goalPos = player.transform.position;
-                rbody.AddForce(new Vector3(0, 1, 0) * 100.0f);
-            }
-        }
-        else if (myState == enemyState.Attack)
-        {
-            if (isAttack)
-            {
-                if (transform.position != goalPos)
-                {
-                    transform.position += forAttackVector(goalPos);
-                }
-                else
-                {
-                    isAttack = false;
-                }
-            }
-            else
-            {
-                myState = enemyState.idle;
-            }
-        }
+        //ìÀêi
+        attack();
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("floor") && myState == enemyState.Attack)
         {
             isAttack = true;
+            anim.SetBool("attack", true);
+            anim.SetBool("find", false);
         }
     }
 
@@ -115,8 +83,106 @@ public class attack_E_SC : MonoBehaviour
     {
         Vector3 returnVec;
 
-        returnVec = Vector3.Normalize(goalPos - transform.position) * 0.03f;
+        returnVec = Vector3.Normalize(goalPos - transform.position) * 0.02f;
 
         return returnVec;
+    }
+
+    private void idle()
+    {
+        if (myState != enemyState.idle)
+        {
+            return;
+        }
+
+        Vector3 pos = transform.position;
+
+        moveCount++;
+        if (moveCount > maxMoveCount)
+        {
+            isTurn = !isTurn;
+            moveCount = 0;
+        }
+
+        if (isTurn)
+        {
+            pos.x += moveSpeed;
+            rotation = new Vector3(0, 90, 0);
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+        else
+        {
+            pos.x -= moveSpeed;
+            rotation = new Vector3(0, -90, 0);
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+
+        transform.position = pos;
+
+        //çıìGÇ…à¯Ç¡Ç©Ç©Ç¡ÇΩÇÁ
+        if (forPlayer.magnitude <= 10.0f)
+        {
+            isFind = true;
+            isAttack = false;
+            anim.SetBool("find", true);
+            anim.SetBool("attack", false);
+            myState = enemyState.Attack;
+            return;
+        }
+    }
+
+    private void attack()
+    {
+        if (myState != enemyState.Attack)
+        {
+            return;
+        }
+
+        if (transform.position.x > player.transform.position.x)
+        {
+            rotation = new Vector3(0, -90, 0);
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+
+        if (transform.position.x < player.transform.position.x)
+        {
+            rotation = new Vector3(0, 90, 0);
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+
+        if (isAttack)
+        {
+            if (animState.IsName("metarig|find") && animState.normalizedTime < 1.0f)
+            {
+                return;
+            }
+
+            if (transform.position.x >= goalPos.x - 0.4f && transform.position.x <= goalPos.x + 0.4f)
+            {
+                isFind = true;
+                isAttack = false;
+                anim.SetBool("find", true);
+                anim.SetBool("attack", false);
+                return;
+            }
+            else
+            {
+                transform.position += forAttackVector(goalPos);
+                return;
+            }
+        }
+
+        if (isFind)
+        {
+            if (animState.IsName("metarig|find") && animState.normalizedTime >= 1.0f)
+            {
+                isFind = false;
+                isAttack = true;
+                anim.SetBool("find", false);
+                anim.SetBool("attack", true);
+                goalPos = player.transform.position;
+                return;
+            }
+        }
     }
 }
