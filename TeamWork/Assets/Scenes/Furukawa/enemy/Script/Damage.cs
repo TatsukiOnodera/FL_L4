7594cf7c582940f876943d8af53adc944b9b10a6
TATSUKI-ONODERA <k020g1153@g.neec.ac.jp>
2024-02-05 21:25:03 +0900

    no message

---
+++ TeamWork/Assets/Scenes/Furukawa/enemy/Script/Damage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// “–‚½‚è”»’è
+    /// </summary>
+    /// <param name="collision">Collision</param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            player_SC playerSC = collision.gameObject.GetComponent<player_SC>();
+            playerSC.damage();
+        }
+    }
+}
